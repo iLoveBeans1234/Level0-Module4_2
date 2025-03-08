bf8bfbd5b74345f79a59a0e9fb3454497d26de48
league <maddoxseul@sbsd.net>
2025-03-07 16:54:09 -0800

    I am happy

--- _03_while_loops/_c_vending_machine.py
+++ _03_while_loops/_c_vending_machine.py
@@ -6,7 +6,7 @@ from tkinter import messagebox, simpledialog, Tk
 
 # Write you code under the if __name__ == '__main__': below
 def vending_machine(money):
-    items_for_sale = {"water" : 0.50, "soda" : 1.00, "pretzels" : 1.00, "candy bar" : 1.50, "exit" : 0.00}
+    items_for_sale = {"water" : 0.50, "soda" : 1.00, "pretzels" : 1.00, "candy bar" : 1.00, "exit" : 0.00}
 
     while True:
         intro_str = "Welcome to the vending machine! You have " +\
@@ -37,16 +37,27 @@ if __name__ == '__main__':
     money_in_dollars = 3.00
 
     # TODO) Write a while loop that ends when you have no money left
-
+    while money_in_dollars > 0:
         # TODO) Call the vending_machine() function and save the money spent
         #  in a variable, for example:
         #  money_spent = vending_machine(money_in_dollars)
-
+        money_spent = vending_machine(money_in_dollars)
         # TODO) If no money was spent, tell the user how much money they still
         #  have and exit the while loop
+        if money_spent == 0:
+            messagebox.showinfo(title="user", message="You still have " + str(money_in_dollars - money_spent))
+            break
+        else:
+            messagebox.showinfo(title='user', message="You still have " + str(money_in_dollars - money_spent))
+            money_in_dollars -= money_spent
+        if money_in_dollars == 0:
+            messagebox.showinfo(title="user", message="Congratulations! You have maximized your money amount!")
+        if money_in_dollars < 0:
+            messagebox.showinfo(title="user", message="You overspent!")
 
-        # TODO) Otherwise, subtract the money spent from the amount of money
-        #  you still have (money_in_dollars)
+
+    # TODO) Otherwise, subtract the money spent from the amount of money
+        #  you still have (money_in_dollars).
 
     # TODO) If there is exactly 0 money left, create a message that
     #  congratulates the user because they maximized their money.
